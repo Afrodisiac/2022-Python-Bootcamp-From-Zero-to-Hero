25606f9bb7b8fe7c4a20da3494532b11c5d71072
Jeremy Stretter <43216926+Afrodisiac@users.noreply.github.com>
2022-06-08 22:22:03 -0700

    Added lesson "Python Numbers"

---
+++ Section 3 - Python Object and Data Structure Basics/Python Numbers.py
@@ -0,0 +1,34 @@
+# Python Numbers - Notes
+
+# Addition (+) operator
+add = 33 + 36
+print(f"The result of 33 + 36 is {add}")
+
+# Subtraction (-) operator
+sub = 421 - 1
+print(f"The result of 421 - 1 is {sub}")
+
+# Multiplication (*) operator
+mult = 8675309 * 1
+print(f"The result of 8675309 * 1 is {mult}")
+
+# Division (/) operator
+div = 200 / 5
+print(f"The result of 200 / 5 is {div}")
+
+# Modulo or "Mod" operator
+#
+# The modulo operator returns the remainder of a division operation.
+mod1 = 7 % 4
+print(f"The remainder of 7 / 4 is {mod1}")  # Returns 3 since 7 is divisible by 4 once, with a remainder of 3.
+
+# Exponent (**) operator
+pow = 3 ** 2
+print(f"3 raised to the 2nd power is {pow}")
+
+# Python also follows PEMDAS. For instance:
+pmds_1 = 2 + 10 * 10 - 3
+print(f"The result of 2 + 10 * 10 - 3 is {pmds_1}")  # returns 99
+
+pmds_2 = (2 + 10) * 10 - 3
+print(f"The result of (2 + 10) * 10 - 3 is {pmds_2}")  # returns 117
