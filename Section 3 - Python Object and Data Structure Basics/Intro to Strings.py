7372f8b9b196ae7ce06d887243d4a30c227dd1e9
Jeremy Stretter <43216926+Afrodisiac@users.noreply.github.com>
2022-06-16 19:29:05 -0700

    Added content to Intro to Strings

--- Section 3 - Python Object and Data Structure Basics/Intro to Strings.py
+++ Section 3 - Python Object and Data Structure Basics/Intro to Strings.py
@@ -11,7 +11,7 @@ from termcolor import colored, cprint
 # Indexing starts at zero. For instance, if you wanted to grab
 # the character "B" from the string below, you would use index [4].
 my_string = "goodbye"
-cprint(f"The character at index [4]  of the {my_string} is {my_string[4]}.", "green",)
+cprint(f"The character at index [4]  of the {my_string} is {my_string[4]}.", "green")
 #
 # If you wanted to grab the last letter of a string, but did not
 # know the length of the string. One option is to use the reverse
@@ -33,3 +33,22 @@ cprint(f"This is the alphabet, with a step of 2: {alpha_string[::2]}", "yellow")
 #
 # Another neat trick is to use reverse indexing to reverse a string:
 cprint(f"Here is the alphabet reversed: {alpha_string[::-1]}", "grey")
+#
+#
+# Immutability
+#
+# Let's say, for instance, you wanted to change the name below from "Pam" to "Sam":
+some_name = "Pam"
+print(f"Hi, {some_name}!")
+# some_name[0] = "S"
+# print(some_name) will produce the following error: TypeError: 'str' object does not support item assignment
+#
+# One way to achieve this with the use of strings is with the use of string concatenation. For instance:
+my_name = "Jeremy"
+spanish_name = cprint(f"Hi, {my_name[0:5]}" + "ias!", "white", "on_red", attrs=["bold"])
+#
+# Other useful functions of strings:
+x = "Hello, World!"
+print(x.upper())
+print(x)  # Using the .upper() function will NOT save. To do that: you will have to assign it to x.
+print(x.split())
